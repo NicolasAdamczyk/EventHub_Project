4be5107793579dee6b46da215d90fa1fe833acf6
Nicolas Adamczyk <NicolasAdamczyk>
2026-03-02 15:46:14 +0100

    Mise en production : Autorisation CORS pour Vercel

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -142,4 +142,5 @@ SPECTACULAR_SETTINGS = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://event-hub-project-five.vercel.app", # Application finale déployée sur Vercel
 ]
